6081c88b8771ce28100457d7266edfca758b81f5
zach <szboatwright@gmail.com>
2019-04-27 18:55:53 -0500

    📦  NEW: player can now 'attack' enemies

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -1,13 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 using RPG.Movement;
+using RPG.Combat;
 
 namespace RPG.Control
 {
   public class PlayerController : MonoBehaviour
   {
     void Update()
+    {
+      InteractWithCombat();
+      InteractWithMovement();
+    }
+
+    private void InteractWithCombat()
+    {
+      RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
+      foreach (RaycastHit hit in hits)
+      {
+        CombatTarget target = hit.transform.GetComponent<CombatTarget>();
+        if (target == null) continue;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+          GetComponent<Fighter>().Attack(target);
+        }
+      }
+    }
+
+    private void InteractWithMovement()
     {
       if (Input.GetMouseButton(0))
       {
@@ -17,14 +40,18 @@ namespace RPG.Control
 
     private void MoveToMousePosition()
     {
-      Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); ;
       RaycastHit hit;
-      bool hasHit = Physics.Raycast(ray, out hit);
+      bool hasHit = Physics.Raycast(GetMouseRay(), out hit);
 
       if (hasHit)
       {
         GetComponent<Mover>().MoveTo(hit.point);
       }
+    }
+
+    private static Ray GetMouseRay()
+    {
+      return Camera.main.ScreenPointToRay(Input.mousePosition);
     }
   }
 
