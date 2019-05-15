f226ca19a150d91ae082ef486d5929fb61ff8032
zach <szboatwright@gmail.com>
2019-05-15 17:22:33 -0500

    ⚡  IMPROVE: characters will now die when their health reaches 0

--- Assets/Scripts/Combat/Health.cs
+++ Assets/Scripts/Combat/Health.cs
@@ -5,11 +5,17 @@ namespace RPG.Combat
   public class Health : MonoBehaviour
   {
     [SerializeField] float health = 100f;
+    bool isDead = false;
 
     public void TakeDamage(float amount)
     {
       health = Mathf.Max(health - amount, 0);
-      print(health);
+
+      if (health <= 0 && !isDead)
+      {
+        GetComponent<Animator>().SetTrigger("die");
+        isDead = true;
+      }
     }
   }
 }
